9099a4f8835b07bfafa9206d4e2ab762e4f2c319
Ramoth <ramothlp@gmail.com>
2015-06-30 09:04:47 +0200

    Update MyDealz Weiterleitung.user.js
    Erweiterung auf Gutscheine und Freebies

--- MyDealz Weiterleitung.user.js
+++ MyDealz Weiterleitung.user.js
@@ -1,17 +1,35 @@
 // ==UserScript==
 // @name         MyDealz Weiterleitung
-// @namespace    http://www.mydealz.de/deals
+// @namespace    http://www.mydealz.de
 // @version      1.0
 // @description  Automatische Weiterleitung von heißen Dealz auf aktuelle Dealz
 // @author       Ramoth
 // @include      http://www.mydealz.de/deals
+// @include      http://www.mydealz.de/gutscheine
+// @include      http://www.mydealz.de/freebies
 // @grant        none
 // ==/UserScript==
 
-var oldPage = "http://www.mydealz.de/deals"; 
-var newPage = "http://www.mydealz.de/deals-new"; 
+var oldDeals = "http://www.mydealz.de/deals"; 
+var newDeals = "http://www.mydealz.de/deals-new"; 
 
-if (document.location.href.indexOf(oldPage) == 0) 
+var oldGutscheine = "http://www.mydealz.de/gutscheine"; 
+var newGutscheine = "http://www.mydealz.de/gutscheine-new"; 
+
+var oldFreebies = "http://www.mydealz.de/freebies"; 
+var newFreebies = "http://www.mydealz.de/freebies-new"; 
+
+if (document.location.href.indexOf(oldDeals) == 0) 
 { 
-    document.location.href=newPage; 
+    document.location.href=newDeals; 
+}  
+
+if (document.location.href.indexOf(oldGutscheine) == 0) 
+{ 
+    document.location.href=newGutscheine; 
+}  
+
+if (document.location.href.indexOf(oldFreebies) == 0) 
+{ 
+    document.location.href=newFreebies; 
 }  
